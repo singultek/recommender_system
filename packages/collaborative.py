84b9ee9d0576c7151719f054c9d35f12b1ccf90c
singultek <sinan.gultekin95@gmail.com>
2021-05-20 17:15:11 +0200

    Adding decide_algorithm method

--- packages/collaborative.py
+++ packages/collaborative.py
@@ -11,20 +11,97 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import pandas as pd
+import numpy as np
+from surprise import Reader
+from surprise import Dataset
+from surprise import accuracy
+from surprise import KNNBaseline, SVD
+from surprise.model_selection import KFold
+from tqdm import tqdm
 
 
-def initialize_collab(movies, ratings, user_id, num_recommendation, algorithm):
-    total_number_of_ratings = ratings.shape[0]
-    total_number_of_movies = ratings[['movieId']].drop_duplicates(['movieId']).shape[0]
-    total_number_of_users = ratings[['userId']].drop_duplicates(['userId']).shape[0]
+class CollabMovies:
+    def __init__(self,
+                 dataset_path='./data',
+                 algorithm='UserUser') -> None:
+        """
+        Initializing the Movies class
+        Args:
+            dataset_path: the string with the path of dataset
+                        By default, ./data folder path is given
+        Return:
+            None
+        """
+        self.dataset_path = dataset_path
 
-    print('\nColumns/Features of Dataset: ', list(ratings.columns))
-    print('Total Number of Ratings : ', total_number_of_ratings)
-    print('Total Number of Movies: ', total_number_of_movies)
-    print('Total Number of Users: ', total_number_of_users)
+        self.create_dataframes(dataset_path)
+        self.movies_list, self.users_list = self.__create_unique_lists(self.ratings_df)
+        self.decide_algorithm(algorithm=algorithm)
+        print(self.selected_algorithm)
 
-    print(movies.columns)
-    print(user_id)
-    print(num_recommendation)
-    print(algorithm)
-    return
+    def create_dataframes(self,
+                          dataset_path: str) -> None:
+        """
+        The method for reading the datasets and creating dataframe
+        Args:
+            dataset_path: the string with the path of dataset
+        Return:
+            None
+        """
+        # Creates movies dataframe
+        movies_path = '{}/movies.csv'.format(dataset_path)
+        self.movies_df = pd.read_csv(movies_path)
+
+        # Creates ratings dataframe
+        ratings_path = '{}/ratings.csv'.format(dataset_path)
+        self.ratings_df = pd.read_csv(ratings_path)
+        self.ratings_df = self.ratings_df.drop('timestamp', axis=1)
+
+    @staticmethod
+    def __create_unique_lists(dataframe: pd.DataFrame) -> (list, list):
+        """
+        The method for getting unique elements of dataframe
+        Args:
+            dataframe: the pd.Dataframe that will be computed to get unique elements
+        Return:
+            dataframe['movieId'].unique(), dataframe['userId'].unique(): the tuple of
+            lists those contain the unique movie and user elements
+        """
+        return dataframe['movieId'].unique(), dataframe['userId'].unique()
+
+    def decide_algorithm(self, algorithm):
+        self.selected_algorithm = ''
+        if algorithm is not None:
+            backbone = algorithm.split('-')[0]
+            if backbone == 'UserUser':
+                print('User-User memory based collaborative approach is selected!')
+                self.selected_algorithm = backbone
+            elif backbone == 'ItemItem':
+                print('Item-Item memory based collaborative approach is selected!')
+                self.selected_algorithm = backbone
+            elif backbone == 'KNN' and len(algorithm.split('-')) == 3:
+                print('KNN model based collaborative approach is selected!')
+                similarity_measure = algorithm.split('-')[1]
+                user_based = algorithm.split('-')[2]
+                if (similarity_measure == 'cosine' or similarity_measure == 'msd' or similarity_measure == 'pearson' or similarity_measure == 'pearson_baseline') and (user_based == 'user'):
+                    self.selected_algorithm = KNNBaseline(sim_options={'name': similarity_measure,
+                                                                       'user_based': True})
+                elif (similarity_measure == 'cosine' or similarity_measure == 'msd' or similarity_measure == 'pearson' or similarity_measure == 'pearson_baseline') and (user_based == 'item'):
+                    self.selected_algorithm = KNNBaseline(sim_options={'name': similarity_measure,
+                                                                       'user_based': False})
+                else:
+                    raise ValueError('The given parameters {} for KNN algorithm is not recognized. Please check the propoer format'.format(str(backbone)+'-'+str(similarity_measure)+'-'+str(user_based)))
+            elif backbone == 'SVD' and len(algorithm.split('-')) == 3:
+                print('SVD model based collaborative approach is selected!')
+                epoch = int(algorithm.split('-')[1])
+                learning_rate = float(algorithm.split('-')[2])
+                self.selected_algorithm = SVD(n_epochs=epoch, lr_all=learning_rate)
+            elif backbone == '':
+                raise ValueError('Please give an algorithm! Algorithm cannot be left empty')
+            else:
+                raise ValueError('The given algorithm format {} is not recognized. Please check the propoer format'.format(str(backbone)))
+        elif algorithm is None:
+            raise ValueError('Please give an algorithm! Algorithm cannot be left empty')
+
+        return self.selected_algorithm
